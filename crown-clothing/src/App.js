e84ea95d71b0a69aaf13a28accebe7711249b750
Andrew Cruz <ajdat10@gmail.com>
2022-01-18 05:57:49 -0500

    adding header component and link to home, shop page and contact

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
-import HomePage from './pages/HomePage/hompage';
-import ShopPage from './pages/Shop/shop';
-import './App.css';
-
+import React from "react";
+import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import HomePage from "./pages/HomePage/hompage";
+import ShopPage from "./pages/Shop/shop";
+import Header from "./components/Header/header";
+import "./App.css";
 
 function App() {
   return (
-    <div >
+    <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
-      
     </div>
   );
 }
