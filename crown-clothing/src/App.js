922b11513326874cc0b0602344ea3b7cb7d67717
Andrew Cruz <ajdat10@gmail.com>
2022-01-19 05:57:55 -0500

    added sign in page and components

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import HomePage from "./pages/HomePage/hompage";
 import ShopPage from "./pages/Shop/shop";
 import Header from "./components/Header/header";
+import SignInAndSignUp from "./pages/SignIn-SignUp/signin-signup";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ crown-clothing/src/pages/SignIn-SignUp/signin-signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../../components/Sign-In/sign-in";
+import "./signin-signup.scss";
+
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUp;
